910662f7a9bbe9922cc45a770eb53dd79fe8a601
BOSS-PC\boss <contact@eplex.eu>
2017-12-21 13:52:14 +0100

    redux c.d.

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,12 +3,18 @@ import {createStore} from 'redux';
 const store = createStore((state = {count: 0}, action) => {
     switch (action.type) {
         case 'INCREMENT':
+            const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
             return {
-                count: state.count + 1
+                count: state.count + incrementBy
             };
         case 'DECREMENT':
+            const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
             return {
-                count: state.count - 1
+                count: state.count - decrementBy
+            };
+        case 'SET':
+            return {
+                count: action.count
             };
         case 'RESET':
             return {
@@ -22,11 +28,17 @@ const store = createStore((state = {count: 0}, action) => {
     return state;
 });
 
-console.log(store.getState());
+const unsubscribe = store.subscribe(() => {
+    console.log(store.getState());    
+});
 
 store.dispatch({
-    type: 'INCREMENT'
+    type: 'INCREMENT',
+    incrementBy: 5
 });
+
+// zatrzymanie nasluchiwania store
+//unsubscribe(); 
 
 store.dispatch({
     type: 'INCREMENT'
@@ -40,4 +52,12 @@ store.dispatch({
     type: 'DECREMENT'
 });
 
-console.log(store.getState());
+store.dispatch({
+    type: 'DECREMENT',
+    decrementBy: 10
+});
+
+store.dispatch({
+    type: 'SET',
+    count: 101
+})
